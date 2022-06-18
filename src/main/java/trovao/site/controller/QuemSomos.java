d9d145235a2b6f9cda26d935bec8004133799562
Willian Pegoraro <pegorarogabriel@hotmail.com>
2022-06-18 17:13:10 -0300

    new updates

---
+++ src/main/java/trovao/site/controller/QuemSomos.java
@@ -0,0 +1,17 @@
+package trovao.site.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/quem-somos")
+public class QuemSomos {
+	
+	@GetMapping
+	public String quemSomos(Model model) {
+		return"quem-somos";
+	}
+	
+}
---
+++ src/main/resources/static/js/swiper-carousel.js
@@ -0,0 +1,37 @@
+var swiper = new Swiper(".mySwiper", {
+  freeMode: true,
+  slidesPerView: "5",
+  spaceBetween: 5,
+  loop: true,
+  speed: 3000,
+  autoplay: {
+
+      //reverseDirection: true,
+    delay: 2000,
+    disableOnInteraction: false,
+    pauseOnMouseEnter: true,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
+
+var swiper = new Swiper(".mySwiperReverse", {
+  freeMode: true,
+  slidesPerView: "5",
+  spaceBetween: 5,
+  loop: true,
+  speed: 3000,
+  autoplay: {
+
+    reverseDirection: true,
+    delay: 2000,
+    disableOnInteraction: false,
+    pauseOnMouseEnter: true,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
